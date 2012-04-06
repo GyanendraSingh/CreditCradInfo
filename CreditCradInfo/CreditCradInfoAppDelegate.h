919b18777b4c82ee184ce0e53b5a33d119754821
Gyanendra Singh <gyanendra@knstek.com>
2012-04-06 00:19:48 -0700

    Initial Commit

---
+++ CreditCradInfo/CreditCradInfoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CreditCradInfoAppDelegate.h
+//  CreditCradInfo
+//
+//  Created by admin on 4/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditCradInfoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CreditCradInfo/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  CreditCradInfo
+//
+//  Created by admin on 4/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CreditCradInfoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([CreditCradInfoAppDelegate class]));
+    }
+    return retVal;
+}
